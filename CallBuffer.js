51dd7a1b2b794a68b96198c27f202d9a1d2196e0
Borislav Peev <borislav.asdf@gmail.com>
2015-10-21 15:13:17 +0200

    fix domain handling problems with buffered calls

--- CallBuffer.js
+++ CallBuffer.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var Domain = require( 'domain' );
+var Array_slice = Array.prototype.slice;
+
+
 // a class that collects all its .write()s for later
 class CallBuffer {
 	
@@ -12,18 +16,26 @@ class CallBuffer {
 	}
 
 	push () {
-		this._buffer.push( arguments[ 0 ], Array.prototype.slice.call( arguments, 1 ) );
+		this._buffer.push( arguments[ 0 ], Array_slice.call( arguments, 1 ), process.domain );
 	}
 
 	unshift () {
-		this._buffer.unshift( arguments[ 0 ], Array.prototype.slice.call( arguments, 1 ) );	
+		this._buffer.unshift( arguments[ 0 ], Array_slice.call( arguments, 1 ), process.domain );	
 	}
 
 	flush ( session ) {
 		var buffer = this._buffer;
+		var originalDomain = process.domain;
 		// apply all writes
-		for ( var i = 0, iend = buffer.length; i < iend; i += 2 ) {
+		for ( var i = 0, iend = buffer.length; i < iend; i += 3 ) {
+			var domain = buffer[ i + 2 ];
+			if ( domain && process.domain !== domain ) {
+				domain.enter();
+			}
 			session[ buffer[ i ] ].apply( session, buffer[ i + 1 ] );
+		}
+		while ( process.domain && process.domain !== originalDomain ) {
+			process.domain.exit();
 		}
 		this._buffer = [];
 	}
